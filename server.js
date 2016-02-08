34231f528fb657fda4be1a60ff5ceab56d6418b8
tom1700 <tomekm1700@gmail.com>
2016-02-08 11:09:38 +0000

    update

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 'use strict';
 
 var express = require('express');
-var routes = require('./app/routes/index.js');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var session = require('express-session');
-var url = require('url');
 var dateConverter = require("./dateConverter");
 
 var app = express();
 require('dotenv').load();
-require('./app/config/passport')(passport);
 
 mongoose.connect(process.env.MONGO_URI);
 
@@ -27,11 +24,13 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-routes(app, passport);
-
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
+});
+
+app.get("/",function(req,res){
+	res.send("Natural languge example 'December 15, 2015'\n Unix example '123121243241234'");
 });
 
 app.get("/:date",function(req,res){
